cbb06c9f67575dc117daf0718b8b6ff979db812c
Mdesignscode <marlonbaatjes@gmail.com>
2024-03-03 20:51:40 +0200

    🏠🖼➕🪡 create home page 🏠🖼➕🪡

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
+import tailwindIntegration from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwindIntegration()
+  ]
+});
